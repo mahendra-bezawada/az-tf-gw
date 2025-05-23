3f3ee55a336f316a0697e6742a08fd48721f966d
mahendra-bezawada <74664058+mahendra-bezawada@users.noreply.github.com>
2025-05-23 15:38:25 -0400

    update appgw config

--- az_appgw/main.tf
+++ az_appgw/main.tf
@@ -29,6 +29,7 @@ resource "azurerm_application_gateway" "appgw" {
   name                = "appgw-demo"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
   sku {
     name     = "Standard_v2"
     tier     = "Standard_v2"
@@ -40,14 +41,14 @@ resource "azurerm_application_gateway" "appgw" {
     subnet_id = azurerm_subnet.subnet.id
   }
 
-  frontend_port {
-    name = "frontendPort"
-    port = 443
-  }
-
   frontend_ip_configuration {
     name                 = "appgw-fe"
     public_ip_address_id = azurerm_public_ip.pip.id
+  }
+
+  frontend_port {
+    name = "frontendPort"
+    port = 443
   }
 
   ssl_profile {
@@ -57,13 +58,40 @@ resource "azurerm_application_gateway" "appgw" {
       policy_name          = var.ssl_profile[0].ssl_policy[0].policy_name
       policy_type          = var.ssl_profile[0].ssl_policy[0].policy_type
       min_protocol_version = coalesce(var.ssl_profile[0].ssl_policy[0].protocol_version, "TLSv1_2")
-      cipher_suites        = var.ssl_profile[0].ssl_policy[0].policy_type == "Custom" ? var.ssl_profile[0].ssl_policy[0].cipher_suites : null
+      cipher_suites        = var.ssl_profile[0].ssl_policy[0].cipher_suites
     }
   }
 
-  lifecycle {
-    ignore_changes = [ssl_profile] # optional, useful for testing
+  backend_address_pool {
+    name = "appgw-backend-pool"
+
+    backend_addresses {
+      ip_address = "10.0.2.4"
+    }
   }
 
-  depends_on = [azurerm_public_ip.pip]
+  backend_http_settings {
+    name                  = "appgw-http-settings"
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 20
+  }
+
+  http_listener {
+    name                           = "appgw-listener"
+    frontend_ip_configuration_name = "appgw-fe"
+    frontend_port_name             = "frontendPort"
+    protocol                       = "Https"
+    ssl_profile_name               = var.ssl_profile[0].name
+  }
+
+  request_routing_rule {
+    name                       = "appgw-routing-rule"
+    rule_type                  = "Basic"
+    http_listener_name         = "appgw-listener"
+    backend_address_pool_name  = "appgw-backend-pool"
+    backend_http_settings_name = "appgw-http-settings"
+  }
 }
+
