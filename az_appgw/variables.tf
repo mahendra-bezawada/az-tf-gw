b348f6cbacb7a16ffa83dfb87a9afd6cd1fc200f
mahendra-bezawada <74664058+mahendra-bezawada@users.noreply.github.com>
2025-05-27 21:34:23 -0400

    Update variables.tf

--- az_appgw/variables.tf
+++ az_appgw/variables.tf
@@ -8,10 +8,10 @@ variable "ssl_profile" {
   description = "AppGateway SSL profile"
   type = list(object({
     name       = string
-    ssl_policy = list(object({
-      policy_name         = string
-      policy_type         = string
-      protocol_version    = string
-    }))
+    ssl_policy = object({
+      policy_name      = string
+      policy_type      = string
+      protocol_version = optional(string)
+    })
   }))
 }
