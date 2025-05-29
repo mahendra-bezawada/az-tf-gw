0b1d7a414c23df26bba0d40201a9e2436b19118d
mahendra-bezawada <74664058+mahendra-bezawada@users.noreply.github.com>
2025-05-28 21:39:09 -0400

    test

--- az_appgw/main.tf
+++ az_appgw/main.tf
@@ -61,14 +61,15 @@ resource "azurerm_application_gateway" "appgw" {
     name = "frontendPort"
     port = 443
   }
-
+/*
   ssl_policy {
       policy_name          = "AppGwSslPolicy20220101" #ssl_profile.value.ssl_policy.policy_name
       policy_type          = "Predefined"
       min_protocol_version = "TLSv1_2" #ssl_profile.value.ssl_policy.min_protocol_version
   }
+  */
 
-/*
+
   dynamic "ssl_profile" {
     for_each = var.ssl_profile
     content {
@@ -80,7 +81,7 @@ resource "azurerm_application_gateway" "appgw" {
       }
     }
 }
-*/
+
 
 
   backend_address_pool {
