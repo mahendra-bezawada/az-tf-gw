16598aecfc92cd1387f2ec31065440439c5d5367
mahendra-bezawada <74664058+mahendra-bezawada@users.noreply.github.com>
2025-05-28 20:07:26 -0400

    typo

--- az_appgw/main.tf
+++ az_appgw/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-appgw"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
-  resource_group_name = resource_group_name
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "subnet" {
